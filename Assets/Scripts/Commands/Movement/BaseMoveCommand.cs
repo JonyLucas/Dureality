42dded9910e645e215d4fafe7145dff0f2570812
João Lucas <jony.lucas.br13@gmail.com>
2022-01-21 20:45:29 -0300

    inclusão das sprites e implementação inicial dos comandos

---
+++ Assets/Scripts/Commands/Movement/BaseMoveCommand.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace Game.Commands.Movement
+{
+    public abstract class BaseMoveCommand : BaseCommand
+    {
+        [SerializeField]
+        protected float speed;
+
+        protected SpriteRenderer renderer;
+        protected Animator animator;
+        //protected PlayerMovement moveScript;
+
+        public abstract string AnimationParameter { get; }
+        protected bool isInitialized = false;
+
+        protected BaseMoveCommand(KeyCode associatedKey, float speedValue) : base(associatedKey)
+        {
+            speed = speedValue;
+        }
+
+        public override void Execute(GameObject gameObject)
+        {
+            if (!isInitialized)
+            {
+                InitializeFields(gameObject);
+            }
+
+            if (ExecutionCodition(gameObject))
+            {
+                ExecuteAction(gameObject);
+            }
+        }
+
+        public virtual void InitializeFields(GameObject gameObject)
+        {
+            renderer = gameObject.GetComponent<SpriteRenderer>();
+            animator = gameObject.GetComponent<Animator>();
+            //moveScript = gameObject.GetComponent<PlayerMovement>();
+        }
+
+        protected abstract void ExecuteAction(GameObject gameObject);
+    }
+}
