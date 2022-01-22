521926daf867eadbd6be11c6736d4274a183f9b3
João Lucas <jony.lucas.br13@gmail.com>
2022-01-21 23:11:56 -0300

    Movimentação do player + implementação base dos comandos

--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -1,29 +1,30 @@
+using Game.Commands.Factories;
 using Game.Commands.Movement;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Game.Player.ScriptableObjects
 {
-    [CreateAssetMenu(menuName = "Player Control", fileName = "Player Control", order = 57)]
+    [CreateAssetMenu(menuName = "Player Control", fileName = "Player Control", order = 55)]
     public class PlayerControl : ScriptableObject
     {
-        //[SerializeField]
-        //private List<MoveCommandFactory> _commands;
+        [SerializeField]
+        private List<MoveCommandFactory> _commands;
 
-        //private List<BaseMoveCommand> _moveCommands;
+        private List<BaseMoveCommand> _moveCommands;
 
-        //public List<BaseMoveCommand> MoveCommands
-        //{ get { return _moveCommands; } }
+        public List<BaseMoveCommand> MoveCommands
+        { get { return _moveCommands; } }
 
-        //private void OnEnable()
-        //{
-        //    _moveCommands = new List<BaseMoveCommand>();
-        //    _commands.ForEach(command => _moveCommands.Add(command.Create()));
-        //}
+        private void OnEnable()
+        {
+            _moveCommands = new List<BaseMoveCommand>();
+            _commands.ForEach(command => _moveCommands.Add(command.Create()));
+        }
 
-        //public void InitializeCommands(GameObject gameObject)
-        //{
-        //    _moveCommands.ForEach(command => command.InitializeFields(gameObject));
-        //}
+        public void InitializeCommands(GameObject gameObject)
+        {
+            _moveCommands.ForEach(command => command.InitializeFields(gameObject));
+        }
     }
 }
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,97 @@
+using Game.Commands.Movement;
+using Game.Player.ScriptableObjects;
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+namespace Game.Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField]
+        private PlayerControl _control;
+
+        //[SerializeField]
+        //private SoundFx _jumpSfx;
+
+        //[SerializeField]
+        //private SoundFx _landingSfx;
+
+        private Rigidbody2D _rigidbody;
+        private Animator _animator;
+        private AudioSource _audioSource;
+
+        public bool IsFacingRight { get; set; } = true;
+        public bool IsMoving { get; set; } = false;
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+            _rigidbody = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            if (Input.anyKey)
+            {
+                var command = _control.MoveCommands
+                    .FirstOrDefault(command => Input.GetKey(command.AssociatedKey));
+
+                if (command != null)
+                {
+                    HorizontalMovement(command);
+                }
+            }
+            else
+            {
+                if (IsMoving)
+                {
+                    StopMovement();
+                }
+            }
+        }
+
+        // Check later
+        private void OnGUI()
+        {
+            Event e = Event.current;
+            if (e.isKey)
+            {
+                Debug.Log("Detected Key: " + e.keyCode);
+            }
+        }
+
+        private void HorizontalMovement(BaseMoveCommand command)
+        {
+            if (command.GetType() == typeof(MoveRightCommand))
+            {
+                command.Execute(gameObject);
+                IsFacingRight = true;
+                IsMoving = true;
+            }
+            else if (command.GetType() == typeof(MoveLeftCommand))
+            {
+                command.Execute(gameObject);
+                IsFacingRight = false;
+                IsMoving = true;
+            }
+            else
+            {
+                StopMovement();
+            }
+        }
+
+        public void StopMovement()
+        {
+            IsMoving = false;
+            _animator.SetBool("isWalking", IsMoving);
+        }
+
+        public void PlayerDeath()
+        {
+            StopMovement();
+            enabled = false;
+        }
+    }
+}
